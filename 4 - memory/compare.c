c33d47506d1a0c0c0ea41c7d037a9727f12c2a02
Chris Bradfield <chris@scribe.net>
2021-10-19 17:11:27 +0000

    updates

---
+++ 4 - memory/compare.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+    if (s == t)
+    {
+        printf("Same\n");
+    }
+    else
+    {
+        printf("Different\n");
+    }
+}
